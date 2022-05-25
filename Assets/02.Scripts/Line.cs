a0ff4fddc0381f4e0eb5e49d71097ccf582e1cf7
MinJi Kang <86655689+100100001001@users.noreply.github.com>
2022-05-25 22:52:20 +0900

    Initial commit

---
+++ Assets/02.Scripts/Line.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+
+    public Transform origin;     // 원점
+    public Transform desination; // 목적지의 위치
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = 0.1f; // 시작 너비
+        lineRenderer.endWidth = 0.1f;   // 끝 너비
+    }
+
+    void Update()
+    {
+        lineRenderer.SetPosition(0, origin.position);
+        lineRenderer.SetPosition(1, desination.position);
+    }
+}
